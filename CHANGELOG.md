505ac8b030828a86124847ab68540278928dfa12
Xumei (Mei) Luo <xluo@ml6349-XLUO.local>
2022-01-04 16:31:46 -0500

    make calculate script more robust

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## Unreleased - 2022-01-04
+- GP-2873 Making RT tests more robust
+
 ## 1.2.1 - 2021-06-03
 - migrate to Vidarr
--- tests/calculate.sh
+++ tests/calculate.sh
@@ -1,1 +1,16 @@
-find . \( -xtype f -size +0 -iname "*.maf.gz" \) -printf "maf file exists\n";
+#!/bin/bash
+set -o nounset
+set -o pipefail
+
+cd $1
+
+module load samtools/1.9 2>/dev/null
+
+ls | sed 's/.*\.//' | sort | uniq -c
+
+find -name *.bam -exec samtools flagstat {} \; | sort
+
+find -name *.bam -exec /bin/bash -c "samtools view {} | md5sum" \; | sort
+
+for v in *.vcf;do cat $v | grep -v ^# | md5sum; done | sort -V
+
